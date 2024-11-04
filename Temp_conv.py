9cf29d6361ed4499a4581d65c90a4203183952f5
Sweeney24 <sweeney24@gmail.com>
2024-11-04 18:37:06 -0500

    add if, elif, else statement for temp unit

--- Temp_conv.py
+++ Temp_conv.py
@@ -1,4 +1,5 @@
 #Temperature Converter Assignment for Prof. Candido's Python class
+from idlelib.run import exit_now
 
 #first write out a welcome message WITH my name
 print("Hello, and welcome to the temperature converter! \nMy name is Andrew and I'll be your guide to converting \
@@ -8,5 +9,15 @@ temperature from Fahrenheit to Celsius. Or Celsius to Fahrenheit")
 tempNumber = float(input('Enter the temperature you wish to be converted: '))
 #Get the unit that the user wants to convert to
 tempUnit = input('Is this Fahrenheit or Celsius? Enter F or C: ')
+#check to see if user's input is acceptable
+if tempUnit == 'F' or tempUnit == 'f':
+    print('You have selected Fahrenheit')
+elif tempUnit == 'C' or tempUnit == 'c':
+    print('You have selected Celsius')
+else:
+    print('Please Enter either F or C')
+    exit_now
+
+#Conversion equations
 
 
