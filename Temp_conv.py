32c5a034e4d899f16e53dce8f8fccad148fab6ce
Sweeney24 <sweeney24@gmail.com>
2024-11-04 17:23:27 -0500

    adding temp converter to git. Initial push.

---
+++ Temp_conv.py
@@ -0,0 +1,5 @@
+#Temperature Converter Assignment for Prof. Candido's Python class
+
+#first write out a welcome message WITH my name
+print("Hello, and welcome to the temperature converter! \nMy name is Andrew and I'll be your guide to converting \
+temperature from Fahrenheit to Celsius. Or Celsius to Fahrenheit")
