f67522777929f58988a7e8161334e090c0c906b7
Sweeney24 <sweeney24@gmail.com>
2024-11-04 17:47:33 -0500

    wrote more lines for temp converter. also pushing compound interest as it's finished.

--- Temp_conv.py
+++ Temp_conv.py
@@ -3,3 +3,10 @@
 #first write out a welcome message WITH my name
 print("Hello, and welcome to the temperature converter! \nMy name is Andrew and I'll be your guide to converting \
 temperature from Fahrenheit to Celsius. Or Celsius to Fahrenheit")
+
+#Get the numerical value the user wants coverted
+tempNumber = float(input('Enter the temperature you wish to be converted: '))
+#Get the unit that the user wants to convert to
+tempUnit = input('Is this Fahrenheit or Celsius? Enter F or C: ')
+
+
