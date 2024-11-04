636de23230bec0e2b14cb7e62a5e5bef6dd2af63
Sweeney24 <sweeney24@gmail.com>
2024-11-04 18:56:14 -0500

    the 'if and or ' statement for upper and lower case is not working correctly - need to look into how to fix.

--- Temp_conv.py
+++ Temp_conv.py
@@ -5,7 +5,7 @@ from idlelib.run import exit_now
 print("Hello, and welcome to the temperature converter! \nMy name is Andrew and I'll be your guide to converting \
 temperature from Fahrenheit to Celsius. Or Celsius to Fahrenheit")
 
-#Get the numerical value the user wants coverted
+#Get the numerical value the user wants converted
 tempNumber = float(input('Enter the temperature you wish to be converted: '))
 #Get the unit that the user wants to convert to
 tempUnit = input('Is this Fahrenheit or Celsius? Enter F or C: ')
@@ -18,6 +18,11 @@ else:
     print('Please Enter either F or C')
     exit_now
 
+if tempNumber > 212 and tempUnit == 'F' or tempUnit == 'f':
+    print('Temp cannot be > 212') #this is exactly how it is written and demanded to be done in the assignment.
+elif tempNumber <= 212 and tempUnit == 'F' or tempUnit == 'f':
+    convCelsius = (5.0/9) * (tempNumber - 32)
+    print('The Celsius equivalent is: ', str(format(convCelsius, ".1f")))
 #Conversion equations
 
 
