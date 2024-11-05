c62643bc393cd69a7165a564a8c79662041922d1
Sweeney24 <sweeney24@gmail.com>
2024-11-04 19:54:26 -0500

    moved some code around and nested an if statement.This should be more concise.

--- Temp_conv.py
+++ Temp_conv.py
@@ -1,5 +1,4 @@
 #Temperature Converter Assignment for Prof. Candido's Python class
-from idlelib.run import exit_now
 
 #first write out a welcome message WITH my name
 print("Hello, and welcome to the temperature converter! \nMy name is Andrew and I'll be your guide to converting \
@@ -16,21 +15,30 @@ elif tempUnit == 'C' or tempUnit == 'c':
     print('You have selected Celsius')
 else:
     print('Please Enter either F or C')
-    exit_now
 
-#conversion of F to C
-if tempNumber > 212 and (tempUnit == 'F' or tempUnit == 'f'): #parentheses here force an accecpt of upper or lower case.
-    print('Temp cannot be > 212') #this is exactly how it is written and demanded to be done in the assignment.
+
+##conversion of F to C## This used to be just a F to C converter, but decided to nest if statements to be more concise.
+#convert either C or F, depending on selection above
+if tempNumber > 212 and (tempUnit == 'F' or tempUnit == 'f') or tempNumber > 100 and (tempUnit == 'C' or tempUnit == 'c'): #parentheses here force an accecpt of upper or lower case.
+    if tempNumber > 212:
+        print('Temp cannot be > 212') #this is exactly how it is written and demanded to be done in the assignment.
+    else:
+        print('Temp cannot be > 100') #this is exactly how it is written and demanded to be done in the assignment.
 elif tempNumber <= 212 and (tempUnit == 'F' or tempUnit == 'f'): #parentheses here force an accecpt of f or F.
     convCelsius = (5.0/9) * (tempNumber - 32)
     print('The Celsius equivalent is: ', str(format(convCelsius, ".1f")))
-#ending the F conversion here since Python will allow to exit without an ELSE. Unlike bash.
-
-#Conversion of C to F
-if tempNumber > 100 and (tempUnit == 'C' or tempUnit == 'c'): #same reason for parentheses as lines 22 and 24
-    print('Temp cannot be > 100')
 elif tempNumber <=100 and (tempUnit == 'C' or tempUnit == 'c'):
     convFahrenheit = ((9.0/5.0) * tempNumber) + 32
     print('The Fahrenheit equivalent is: ', str(format(convFahrenheit, ".1f")))
+#ending the F conversion here since Python will allow to exit without an ELSE. Unlike bash.
+
+#BELOW HERE IS OLD CODE - DECIDED TO MAKE IT MORE STREAMLINED. TO BE IN LINE WITH RUBRIC.
+
+#Conversion of C to F
+# if tempNumber > 100 and (tempUnit == 'C' or tempUnit == 'c'): #same reason for parentheses as lines 22 and 24
+#     print('Temp cannot be > 100')
+# elif tempNumber <=100 and (tempUnit == 'C' or tempUnit == 'c'):
+#     convFahrenheit = ((9.0/5.0) * tempNumber) + 32
+#     print('The Fahrenheit equivalent is: ', str(format(convFahrenheit, ".1f")))
 #ending C conversion here for same reason as line 27
 
