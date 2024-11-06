ca0f24634fee295ed8f2acadf930d932e8777162
Sweeney24 <sweeney24@gmail.com>
2024-11-06 15:28:47 -0500

    added grade letters and exiting out if non-allowed inputs given.

--- grade_analyzer.py
+++ grade_analyzer.py
@@ -1,15 +1,16 @@
 #Grade analyzer for Prof. Candido's python class
-
+#ask for whom we are grading
+uName = input('What is the name of the person we are calculating grades for? ')
 #ask the user the grades to be analyzed
 uFirst_test = int(input('First test score: '))
 uSec_test = int(input('Second test score: '))
 uThird_test = int(input('Third test score: '))
 uFourth_test = int(input('Fourth test score: '))
-#ask user if dropping lowest score
-uDrop_test = input('Drop the lowest test score? Y or N: ')
+
+uDrop_test = input('Drop the lowest test score? Y or N: ') #ask user if dropping lowest score
 uMin = 0  # variable that changes based on lowest grade
 
-NO_DROP_TEST = (uFirst_test + uSec_test + uThird_test + uFourth_test) / 4 #Constant here as there is no need for additional logic
+NO_DROP_TEST = (uFirst_test + uSec_test + uThird_test + uFourth_test) / 4 #Constant
 
 if uFirst_test >=0 and uSec_test >=0 and uThird_test >=0 and uFourth_test >=0:
     if uDrop_test != 'Y' and uDrop_test != 'N' and uDrop_test != 'y' and uDrop_test != 'n':
@@ -47,12 +48,42 @@ if uFirst_test >=0 and uSec_test >=0 and uThird_test >=0 and uFourth_test >=0:
                         uMin = uFourth_test
                         #print(uMin,"Fourth min",YES_DROP_TEST)
             YES_DROP_TEST = (uFirst_test + uSec_test + uThird_test + uFourth_test - uMin) / 3  # Constant here
-            print('correct',YES_DROP_TEST)
+            print(uName,' received a ',format(YES_DROP_TEST, ".1f"))
     else:
         print("no",NO_DROP_TEST)
 else:
     print('Test score cannot be less than 0')
     exit()
+
+#if elif else statements for grade letter.
+if YES_DROP_TEST >= 97.0 or NO_DROP_TEST >= 97.0:
+    print('Letter grade average is A+')
+elif YES_DROP_TEST >= 94.0 or NO_DROP_TEST >= 94.0:
+    print('Letter grade average is A')
+elif YES_DROP_TEST >= 90.0 or NO_DROP_TEST >= 90.0:
+    print('Letter grade average is A-')
+elif YES_DROP_TEST >= 87.0 or NO_DROP_TEST >= 87.0:
+    print('Letter grade average is B+')
+elif YES_DROP_TEST >= 84.0 or NO_DROP_TEST >= 84.0:
+    print('Letter grade average is B')
+elif YES_DROP_TEST >= 80.0 or NO_DROP_TEST >= 80.0:
+    print('Letter grade average is B-')
+elif YES_DROP_TEST >= 77.0 or NO_DROP_TEST >= 77.0:
+    print('Letter grade average is C+')
+elif YES_DROP_TEST >= 74.0 or NO_DROP_TEST >= 74.0:
+    print('Letter grade average is C')
+elif YES_DROP_TEST >= 70.0 or NO_DROP_TEST >= 70.0:
+    print('Letter grade average is C-')
+elif YES_DROP_TEST >= 67.0 or NO_DROP_TEST >= 67.0:
+    print('Letter grade average is D+')
+elif YES_DROP_TEST >= 64.0 or NO_DROP_TEST >= 64.0:
+    print('Letter grade average is D')
+elif YES_DROP_TEST >= 60.0 or NO_DROP_TEST >= 60.0:
+    print('Letter grade average is D-')
+else:
+    print('Letter grade average is F')
+
+
 
 
 # calc_Drop_uFirst = (uSec_test + uThird_test + uFourth_test) / 3
