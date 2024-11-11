f85872b022129b819f93da0a73dd7639f6298cbe
Sweeney24 <sweeney24@gmail.com>
2024-11-11 18:38:05 -0500

    created while loops and updated logic. Getting incorrect answers...

--- Comp_Int_loops.py
+++ Comp_Int_loops.py
@@ -9,34 +9,42 @@ uIntRate = float(input('What is the interest rate: ')) #the interest rate
 while uIntRate <= 0:
     print ('Please enter a positive number')
     uIntRate = float(input('What is the annual interest rate: '))
-uMonths = float(input('How many months: ')) #amount of months
+uMonths = float(input('How many months to display: ')) #amount of months
 while uMonths <= 0:
     print('Please enter a positive number')
-    uMonths = float(input('How many months: '))
-
+    uMonths = float(input('How many months to display: '))
 uGoal = float(input('What is the goal amount?'))
 while uGoal <= 0:
     print('Please enter a positive number')
     uGoal = float(input('What is the goal amount?'))
 
 #convert to decimal
-real_interest = uIntRate / 100
+real_interest = float(uIntRate / 100) # get decimal for percent interest
 
-uTime = (uMonths / 12)
+#uTime = (uMonths / 12) # the decimal interest of the goal # this is incorrect
+# print (uTime)
+#
 
-uAccount_balance = uPrinciple * (1 + real_interest / 12) ** (12 * uTime)
-print(format(uAccount_balance, ".2f"))
+
+#uAccount_balance = uPrinciple * (1 + real_interest / 12) ** (12 * uTime) #incorrect logic
+#uAccount_balance = uPrinciple * (1 + real_interest / 12) ** (12 * uMonths) #testing with umonths
+#print(format(uAccount_balance, ".2f"))
 uDisp_Month = 0
-while uAccount_balance < uGoal:
-    uMonths = uMonths + 1
-    uTime = (uMonths / 12)
 
-    for integer in range(1,13):
-        uDisp_Month += 1
-        uAccount_balance = uPrinciple * (1 + real_interest / 12) ** (12 * uTime)
-        print(uDisp_Month, format(uAccount_balance, ".2f"))
-
-
-#compute# following the order of operations.
-#FinalValue = uPrinciple * (1 + real_interest / uMonth_int) ** (uMonth_int * uMonths)
-#print('The expected value of your investment after', str(uMonths), 'months is','$',str(format(FinalValue,",.2f")))
+while uPrinciple < uGoal:
+    # uMonths = uMonths + 1
+    #uTime = (uMonths / 12)
+    uDisp_Month += 1
+    #uAccount_balance = uPrinciple * (1 + real_interest / 12) ** (12 * uTime)
+    #uAccount_balance = uPrinciple * (1 + real_interest / 12) ** (12 * uMonths) #testing with umonths
+    uAccount_balance_interest = uPrinciple * ((1 + real_interest) / 12)
+    uPrinciple = uPrinciple + uAccount_balance_interest
+    #uMonths = uMonths + 1
+    while uDisp_Month <= uMonths:
+        print('Month', uDisp_Month,': $ ', format(uPrinciple, '.2f') )
+    print('Month', uDisp_Month, format(uPrinciple, ".2f"))
+#
+#
+# #compute# following the order of operations.
+# #FinalValue = uPrinciple * (1 + real_interest / uMonth_int) ** (uMonth_int * uMonths)
+# #print('The expected value of your investment after', str(uMonths), 'months is','$',str(format(FinalValue,",.2f")))
