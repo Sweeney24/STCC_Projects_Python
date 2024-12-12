4e86f5e0b8476ff245b3d28d6f65981a9c22456c
Andrew Sweeney <sweeney24@gmail.com>
2024-12-11 20:58:33 -0500

    extensively commented code. wrote out a bit more logic. created median() function. wrote simple math with builtin functions to produce output.

--- realestate_analyzer.py
+++ realestate_analyzer.py
@@ -13,18 +13,40 @@ def getFloatInput(prompt): #this function is to check that inputs are positive n
     except ValueError: #if the input cannot turn into a float, this is called
       print("Invalid input. Please enter a valid number.")
 
+def getMedian(user_list): #function to find median
+    user_list.sort()#bringing in the value input into the function and sorting it, lower value to highest
+    if len(user_list) % 2 == 0:#determining if the list length is even with modulo
+        #the below code uses the builtin int() and len() functions to make accessing the index viable.
+        middle1 = user_list[int(len(user_list) / 2) - 1] #variable for the middle number, by accessing the index in the middle of the list, minus 1.
+        middle2 = user_list[int(len(user_list) / 2)] #variable for the next middle number, again by accessing the index.
+        median = (middle1 + middle2) / 2 #simple math using the stored variables to find median.
+    else:
+        median = user_list[int(len(user_list) / 2)] #simply assigning median here by accessing the index at the middle with simple math.
+    return median
+
 def main():
     uRealEstateValues = [] #empty list to be populated by loops below
-    while True:
-        uSalesPrice = getFloatInput("Enter in the sales value")
-        uRealEstateValues.append(uSalesPrice)
-        uAnotherValue = input("Enter another value Y or N: ")
+    while True: #continues looping until not true, or it reaches the builtin break.
+        uSalesPrice = getFloatInput("Enter in the sales value:")
+        uRealEstateValues.append(uSalesPrice) #using builtin .append() function to append to the list in this function.
+        uAnotherValue = input("Enter another value yes or no: ")
+        # loop to evaluate user input to see if it DOES NOT match the tuple and uses the builtin function to automatically make the input lower case
         while uAnotherValue.lower() not in ("y", "yes", "n", "no"):
             uAnotherValue = input("invalid entry, enter (y)es or (n)o")
         if uAnotherValue.lower() == "n":
-            break
+            break #breaks this lower loop, returns to the upper loop of getting another sales price
 
 
-    uRealEstateValues.sort()
+    uRealEstateValues.sort() #use the builtin function to sort the list
+    uPropertyNumber = 0 #declare this variable so it's value can increase through each iteration of the for loop below
+    for saledata in uRealEstateValues:
+        uPropertyNumber += 1 #increase value of variable
+        print(f'Property {uPropertyNumber}: ${saledata}') #print out the individual value of each property entered
+    print(f'Minimum: ${uRealEstateValues[0]}') #print out the minimum by accessing the first index
+    print(f'Maximum ${uRealEstateValues[-1]}') #print out the maximum by accessing the last index
+    print(f'Total: ${sum(uRealEstateValues)}') #print the sum using the builtin func
+    print(f'Average: ${sum(uRealEstateValues) / len(uRealEstateValues)}') #print the average using builtin and simple math
+    print(f'Median: ${getMedian(uRealEstateValues)}') #print median using the function specific to this class
+    print(f'Commission: ${sum(uRealEstateValues) * .03}') #print the commission by using builtin and simple math
 
 main()
